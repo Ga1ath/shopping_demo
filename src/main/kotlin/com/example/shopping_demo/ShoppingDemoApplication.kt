024bdaf8d90fea9fd7f139f9c5b0509246355581
Ga1ath <vasjanovich_dmitry@mail.ru>
2022-04-03 20:00:59 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/shopping_demo/ShoppingDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.shopping_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingDemoApplication>(*args)
+}
